d6e520d14497495e3b66020a0faeeeb76250cdbb
Fer2103 <fernandaguilar2103@gmail.com>
2021-11-08 23:41:57 -0400

    Para que vayan probando lo que tienen que hacer, luego lo mejoro

---
+++ Demo/src/demo/Demo.java
@@ -0,0 +1,17 @@
+package demo;
+
+/**
+ *
+ * @author usuario
+ */
+public class Demo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new Pantalla().setVisible(true);
+        // TODO code application logic here
+    }
+    
+}
